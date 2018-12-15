55ef911edb8d16644c6925136a1dbbba3c62b118
hongqi.gan <hongqi.gan@qunar.com>
2018-12-15 21:04:14 +0800

    feat: postcss-config stylelint

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,10 @@
 import './index.css';
 import { formatter } from '@common/date';
 
-console.log('hahaha', formatter(new Date()));
+console.log('hahaha hehe', formatter(new Date()));
 
 let div = document.createElement('div');
-div.innerHTML = 'hello world';
+div.innerHTML = 'hello world kdfjafj';
 
 document.body.append(div);
+
